14e29b54072ae5408bbda08f54ce442ef3a7cfeb
Bia <biaplutarco@gmail.com>
2020-07-17 23:39:22 -0300

    create movie model

---
+++ FaMulan/Networking/Model/Movie.swift
@@ -0,0 +1,35 @@
+//
+//  Movie.swift
+//  FaMulan
+//
+//  Created by Bia Plutarco on 17/07/20.
+//  Copyright © 2020 Bia Plutarco. All rights reserved.
+//
+
+import Foundation
+
+struct Movie: Codable, Equatable {
+
+    var id: Int
+    var title: String
+    var posterPath: String?
+    var popularity: Double
+    var releaseDate: String
+    var voteCount: Int
+    var genres: [Genre]
+
+    enum CodingKeys: String, CodingKey {
+
+        case id
+        case title
+        case posterPath = "poster_path"
+        case popularity
+        case releaseDate = "release_date"
+        case voteCount = "vote_count"
+        case genres
+    }
+
+    static func == (lhs: Movie, rhs: Movie) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
