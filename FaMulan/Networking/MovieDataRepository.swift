c74695858f74b24a65f5b128fd4338a87af160ff
Bia <biaplutarco@gmail.com>
2020-07-18 14:26:31 -0300

    implemented movie repository protocol

---
+++ FaMulan/Networking/MovieDataRepository.swift
@@ -0,0 +1,28 @@
+//
+//  MovieDataRepository.swift
+//  FaMulan
+//
+//  Created by Bia Plutarco on 18/07/20.
+//  Copyright © 2020 Bia Plutarco. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+struct MovieDataRepository: MovieRepository {
+
+    private var provider: Provider<TMDB> {
+
+        let provider = MoyaProvider<TMDB>()
+
+        return Provider<TMDB>(provider: provider)
+    }
+
+    func loadDetails(of movieID: String, completion: @escaping (Result<Movie, APIError>) -> Void) {
+        provider.request(target: .movie(.details(id: movieID)), completion: completion)
+    }
+    
+    func loadMoviesSimilar(to movieID: String, completion: @escaping (Result<Movie, APIError>) -> Void) {
+        provider.request(target: .movie(.similarMovies(to: movieID)), completion: completion)
+    }
+}
