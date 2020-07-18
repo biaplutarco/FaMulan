de93a6fd7f41a955bd3da2f5c18ea034ffeed2b8
Bia <biaplutarco@gmail.com>
2020-07-18 14:41:17 -0300

    reorganize networking folder

--- FaMulan/ViewController.swift
+++ FaMulan/ViewController.swift
@@ -7,11 +7,23 @@
 //
 
 import UIKit
+import Moya
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let repository = MovieDataRepository()
+        var movies = [Movie]()
+        
+        repository.loadDetails(of: Constants.TMDB.mulanID) { result in
+            switch result {
+            case .failure(let error):
+                print(error.localizedDescription)
+            case .success(let movie):
+                movies.append(movie)
+            }
+        }
     }
 }
-
