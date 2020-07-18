b51d9ff7e5fb1b623d9ea0c32814740bc08eacac
Bia <biaplutarco@gmail.com>
2020-07-18 17:02:58 -0300

    create movie detail view

---
+++ FaMulan/Movie Details/MovieDetailsView.swift
@@ -0,0 +1,47 @@
+//
+//  MovieDetailsView.swift
+//  FaMulan
+//
+//  Created by Bia Plutarco on 18/07/20.
+//  Copyright © 2020 Bia Plutarco. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsView: UIView {
+
+    private var imageView = UIImageView()
+
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .clear
+        tableView.separatorStyle = .none
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        return tableView
+    }()
+
+    private var viewModel: MovieDetailsViewModel
+
+    init(viewModel: MovieDetailsViewModel, frame: CGRect) {
+        self.viewModel = viewModel
+
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension MovieDetailsView: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRows
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+}
